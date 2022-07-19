c572808b263b041b0a8c8d61d69e1a2bd8af6092
SamHulme1 <samhulmephotography@gmail.com>
2022-07-19 18:43:59 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,28 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert('You Clicked Submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You Clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
 });
 /**
  * The main game loop called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // create 2 random numbers between 1 and 25
-    let num1 = math.floor(math.random() * 21) +1;
-    let num2 = math.floor(math.random() * 21) +1;
+    let num1 = Math.floor(Math.random() * 21) +1;
+    let num2 = Math.floor(Math.random() * 21) +1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -38,7 +47,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
