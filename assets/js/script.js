871e4eb003a840d4aa4f67fe81031ba32715b50e
SamHulme1 <samhulmephotography@gmail.com>
2022-07-19 16:51:29 +0000

    added event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener('click', function(){
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You Clicked Submit');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You Clicked ${gameType}`);
+            }
+        })
+    }
+});
+
 function runGame(){
 
 }
@@ -31,5 +46,5 @@ function displayMultiplicationQuestion(){
 }
 
 function displayDivisionQuestion(){
-    
+
 }
