97ecafd18479479c579a022055e935598016114b
SamHulme1 <samhulmephotography@gmail.com>
2022-07-19 19:52:09 +0000

    added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,10 @@ function runGame(gameType){
 
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if(gameType==="multiply"){
+        displayMultiplicationQuestion(num1,num2);
+
+    }else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -62,9 +65,12 @@ function calculateCorrectAnswer(){
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById('operator').innerText;
 
-    if (operator = '+') {
+    if (operator === '+') {
         return [operand1 + operand2, 'addition'];
-    } else {
+    } else if(operator === "x"){
+        return [operand1 * operand2, 'multiply'];
+    }
+    else {
         alert(`unimplimented operator ${operator}`);
         throw `unimplimented operator ${operator}. Aborting!`;
     }
@@ -94,14 +100,17 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1,operand2){
 
 }
 
-function displayMultiplicationQuestion(){
+function displayMultiplicationQuestion(operand1,operand2){
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion(){
+function displayDivisionQuestion(operand1,operand2){
 
 }
