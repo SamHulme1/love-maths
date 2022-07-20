f4d17d443eefa85df1ecedbae25217c88e802c43
SamHulme1 <samhulmephotography@gmail.com>
2022-07-20 18:35:21 +0000

    created the code for the division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runGame(gameType){
 
     } else if( gameType==="subtract"){
         displaySubtractQuestion(num1, num2);
+    } else if(gameType==='division'){
+        displayDivisionQuestion(num1,num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -83,6 +85,8 @@ function calculateCorrectAnswer(){
     }
     else if(operator==='-') {
         return[operand1 - operand2, 'subtract'];
+    } else if (operator ='/'){
+        return[operand1 / operand2, 'division'];
     }
     else {
         alert(`unimplimented operator ${operator}`);
@@ -129,6 +133,9 @@ function displayMultiplicationQuestion(operand1,operand2){
 }
 
 function displayDivisionQuestion(operand1,operand2){
-
+    document.getElementById('operand1').textContent = operand1 % operand2 ===0 ? operand1 : operand2 * operand1;
+    document.getElementById('operand2').textContent = operand1 % operand2 ===0 ? operand1: operand1;
+    document.getElementById('operator').textContent = "/";
+    
     
 }
